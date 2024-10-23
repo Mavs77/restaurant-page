1c7019272d01d92fcb50913698f4a2163558d3eb
Laurent Tshisekedi <Laurent.N.Tshisekedi@gmail.com>
2024-10-22 22:44:03 -0500

    added css feature

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import "./styles.css"; 
 
+import cover from './assets/cover-art.jpg'
+
+const imgElement = document.createElement('img'); 
+imgElement.src = cover; 
+document.body.appendChild(imgElement)
+
 console.log('connection is set!!!')
 
 
